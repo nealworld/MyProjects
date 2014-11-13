a90509982e46a9cb764c98561e586d11cbfe24c4
neal cao <nealworld.cao@gmail.com>
2014-11-13 16:32:45 +0800

    second commit
    finish Binding

--- ElectronicLogbook/Source/AircraftEquipment/AircraftEquipmentControl.xaml.cs
+++ ElectronicLogbook/Source/AircraftEquipment/AircraftEquipmentControl.xaml.cs
@@ -46,13 +46,24 @@ namespace ElectronicLogbook.AircraftEquipment
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             lSubEquipment = new SubEquipment();
             lSubEquipment.mEquipmentID = "Equipment_ID: 2";
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1", "1", "1", "1", "1"));
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_11", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_21", "2", "2", "2", "2"));
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             lSubEquipment = new SubEquipment();
             lSubEquipment.mEquipmentID = "Equipment_ID: 3";
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1", "1", "1", "1", "1"));
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_111", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_211", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_111", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_211", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_111", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_211", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_111", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_211", "2", "2", "2", "2"));
+            lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
+            lSubEquipment = new SubEquipment();
+            lSubEquipment.mEquipmentID = "Equipment_ID: 4";
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_114s1", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_214s1", "2", "2", "2", "2"));
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             myList.Add(lAirCraftEquipmentConfig1);
 
@@ -61,22 +72,27 @@ namespace ElectronicLogbook.AircraftEquipment
             lAirCraftEquipmentConfig1.mSubEquipmentList = new List<SubEquipment>();
             lSubEquipment = new SubEquipment();
             lSubEquipment.mEquipmentID = "Equipment_ID: 1";
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1", "1", "1", "1", "1"));
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1222", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2222", "2", "2", "2", "2"));
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             lSubEquipment = new SubEquipment();
             lSubEquipment.mEquipmentID = "Equipment_ID: 2";
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1", "1", "1", "1", "1"));
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1333", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2333", "2", "2", "2", "2"));
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             lSubEquipment = new SubEquipment();
             lSubEquipment.mEquipmentID = "Equipment_ID: 3";
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1", "1", "1", "1", "1"));
-            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2", "2", "2", "2", "2"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_1444", "1", "1", "1", "1"));
+            lSubEquipment.mHWPartList.Add(new HWPart("HW_PART_2444", "2", "2", "2", "2"));
             lAirCraftEquipmentConfig1.mSubEquipmentList.Add(lSubEquipment);
             myList.Add(lAirCraftEquipmentConfig1);
 
             return myList;
+        }
+
+        private void Expander_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+
         }
     }
 }
