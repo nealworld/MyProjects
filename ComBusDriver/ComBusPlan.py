5bd6892a4ff976ac329a4a56314bfc868d69dc18
neal cao <nealworld.cao@gmail.com>
2014-11-15 11:18:21 +0800

    add ComBusDriver

---
+++ ComBusDriver/ComBusPlan.py
@@ -0,0 +1,15 @@
+class Driver:
+	bool dayoff
+	int shitPreference
+	bool dayoffPreference
+	def __init__(sefl):
+		self.dayoff = 0
+		self.shitPreference = -1
+		self.dayoffPreference = 0
+	def __init__(self, aDayoff, aShitPreference, aDayoffPreference):
+		self.dayoff = aDayoff
+		self.shitPreference = aShitPreference
+		self.dayoffPreference = aDayoffPreference 
+
+
+		
